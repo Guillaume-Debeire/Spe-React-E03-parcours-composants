6cb3ff7374a590e332c41796389c40f161ac11b7
guillaume-debeire <guillaume@debeire.com>
2021-06-14 13:06:38 +0200

    import 14/06/2021 from master

---
+++ src/exo1/hello.js
@@ -0,0 +1,14 @@
+export const coucou = () => 'coucou';
+
+export const hello = () => 'hello';
+
+const salut = () => 'salut';
+export default salut;
+
+// on aurait pu faire ceci
+// const coucou = () => 'coucou';
+
+// const hello = () => 'hello';
+
+// export default () => 'salut';
+// export { coucou, hello };
